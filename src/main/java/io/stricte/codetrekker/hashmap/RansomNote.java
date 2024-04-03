67fe15b8fa14986c2781a47d1a1556305d7e8ab4
Slawek Mazur <mazursllawek@gmail.com>
2024-04-03 20:45:02 +0200

    RansomNote

---
+++ src/main/java/io/stricte/codetrekker/hashmap/RansomNote.java
@@ -0,0 +1,20 @@
+package io.stricte.codetrekker.hashmap;
+
+class RansomNote {
+
+  boolean canConstruct(String ransomNote, String magazine) {
+    int[] map = new int[26];
+
+    for (char c : magazine.toCharArray()) {
+      map[c - 'a']++;
+    }
+
+    for (char c : ransomNote.toCharArray()) {
+      if (map[c - 'a'] == 0) {
+        return false;
+      }
+      map[c - 'a']--;
+    }
+    return true;
+  }
+}
